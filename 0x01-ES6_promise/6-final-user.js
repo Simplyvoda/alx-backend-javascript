82d171b4f11002964e59a9fb9d1532bbd0aff1bb
Simplyvoda <efem.vodina@gmail.com>
2023-12-02 23:40:54 +0000

    formatting task 6 and 7 with eslint and prettier

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,11 +2,17 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 async function handleProfileSignup(firstName, lastName, fileName) {
-  const result = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  const result = await Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]);
 
   return result.map((promiseResult) => ({
     status: promiseResult.status,
-    value: promiseResult.status === 'fulfilled' ? promiseResult.value : String(promiseResult.reason),
+    value:
+      promiseResult.status === 'fulfilled'
+        ? promiseResult.value
+        : String(promiseResult.reason),
   }));
 }
 
