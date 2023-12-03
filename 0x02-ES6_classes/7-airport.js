800bd3a9909333f25155690b59fb22810eb95b50
Simplyvoda <efem.vodina@gmail.com>
2023-12-03 13:33:08 +0000

    task 7 and 8

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,10 +1,10 @@
 export default class Airport {
   constructor(name, code) {
-    if (typeof code !== 'string') {
-      throw new Error('Code must be a string');
+    if (typeof code !== "string") {
+      throw new Error("Code must be a string");
     }
-    if (typeof name !== 'string') {
-      throw new Error('Name must be a string');
+    if (typeof name !== "string") {
+      throw new Error("Name must be a string");
     }
 
     this._name = name;
---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,20 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new Error('Size must be a number');
+    }
+    if (typeof location !== 'string') {
+      throw new Error('Location must be a string');
+    }
+    this._size = size;
+    this._location = location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+}
