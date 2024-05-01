e5e032ac53b8fc7d34d45c88c1c726bad48bdab2
Simplyvoda <efem.vodina@gmail.com>
2024-05-01 16:12:07 +0000

    task 5 added

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,77 @@
+/**
+ * Counts the number of students in the database asynchronously.
+ * @param {string} path The path to the CSV data file.
+ * @returns {Promise<void>} A Promise that resolves when the operation is complete.
+ */
+
+const http = require('http');
+
+const app = http.createServer();
+
+const fs = require('fs');
+
+const countStudents = (path) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+	const reportParts = [];
+        const lines = data.trim().split('\n');
+        reportParts.push(`Number of students: ${lines.length - 1}`);
+        const obj = {};
+        lines.forEach((line, index) => {
+          if (index !== 0) {
+            const separated = line.split(",");
+            const firstName = separated[0];
+            const field = separated[separated.length - 1].trim();
+            if (!obj[field]) {
+              obj[field] = [];
+            }
+            obj[field].push(firstName);
+          }
+        });
+        for (const [key, value] of Object.entries(obj)) {
+          const studentList = value.join(', ');
+          reportParts.push(`Number of students in ${key}: ${value.length}. List: ${studentList}`);
+        }
+        resolve(reportParts.join('\n'));
+      }
+    });
+  });
+};
+
+
+app.on('request', (req, res) => {
+	if (req.url === '/') {
+		  const responseText = 'Hello Holberton School!';
+		
+		res.setHeader('Content-Type', 'text/plain');
+		res.setHeader('Content-Length', responseText.length);
+		res.statusCode = 200;
+		res.write(Buffer.from(responseText));
+	}else if (req.url === '/students') {
+		const responseParts = ['This is the list of our students'];
+		countStudents(process.argv[2])
+		.then((report) => {
+			responseParts.push(report);
+			const responseText = responseParts.join('\n');
+			res.setHeader('Content-Type', 'text/plain');
+			res.setHeader('Content-Length', responseText.length);
+			res.statusCode = 200;
+			res.write(Buffer.from(responseText));
+		})
+		.catch((err) => {
+			res.statusCode = 500;
+		});
+	}else{
+		res.statusCode = 400;
+	}
+});
+
+const PORT = 1245;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}/`);
+});
+
+module.exports = app;
