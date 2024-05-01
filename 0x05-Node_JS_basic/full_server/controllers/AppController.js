5225f546c63ea57cbe6e9eac45fbb00b01be8edd
Vodina  Efem <efem.vodina@gmail.com>
2024-05-01 10:10:18 -0700

    Update AppController.js

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,12 @@
+/**
+ * Contains the miscellaneous route handlers.
+ */
+
+class AppController {
+  static getHomepage(req, res) {
+    res.status(200).send('Hello Holberton School!');
+  }
+}
+
+export default AppController;
+module.exports = AppController;
