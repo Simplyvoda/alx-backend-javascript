133208abc9227c02fe26b274f0650d7ad2cd4a5e
Vodina  Efem <efem.vodina@gmail.com>
2024-05-01 08:22:14 -0700

    Update 3-read_file_async.js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,39 +1,35 @@
 const fs = require('fs');
 
 /**
- * Counts the number of students in the database
+ * Counts the number of students in the database asynchronously.
  * @param {string} path The path to the CSV data file.
+ * @returns {Promise<void>} A Promise that resolves when the operation is complete.
  */
-
 const countStudents = (path) => {
-  fs.stat(path, (err, stats) => {
-    if (err) {
-      throw new Error('Cannot load the database');
-    }
-    if (!stats.isFile()) {
-      throw new Error('Cannot load the database');
-    }
+  return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf-8', (err, data) => {
       if (err) {
-        throw new Error('Error reading the file');
-      }
-      const lines = data.trim().split('\n');
-      console.log(`Number of students: ${lines.length - 1}`);
-      const obj = {};
-      lines.forEach((line, index) => {
-        if (index !== 0) {
-          const separated = line.split(",");
-          const firstName = separated[0];
-          const field = separated[separated.length - 1].trim();
-          if (!obj[field]) {
-            obj[field] = [];
+        reject(new Error('Cannot load the database'));
+      } else {
+        const lines = data.trim().split('\n');
+        console.log(`Number of students: ${lines.length - 1}`);
+        const obj = {};
+        lines.forEach((line, index) => {
+          if (index !== 0) {
+            const separated = line.split(",");
+            const firstName = separated[0];
+            const field = separated[separated.length - 1].trim();
+            if (!obj[field]) {
+              obj[field] = [];
+            }
+            obj[field].push(firstName);
           }
-          obj[field].push(firstName);
+        });
+        for (const [key, value] of Object.entries(obj)) {
+          const studentList = value.join(', ');
+          console.log(`Number of students in ${key}: ${value.length}. List: ${studentList}`);
         }
-      });
-      for (const [key, value] of Object.entries(obj)) {
-        const studentList = value.join(', ');
-        console.log(`Number of students in ${key}: ${value.length}. List: ${studentList}`);
+        resolve();
       }
     });
   });
