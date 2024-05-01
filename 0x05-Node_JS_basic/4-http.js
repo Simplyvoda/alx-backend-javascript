3654b9ead8cf29bb0d48153e56b48ea9e8738b61
Vodina  Efem <efem.vodina@gmail.com>
2024-05-01 07:12:54 -0700

    Update 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,11 +1,14 @@
 const http = require('http');
 
-const app = http.createServer((req, res) => {
-	res.setHeader('Content-Type', 'text/plain');
-	res.statusCode = 200;
-	res.end('Hello Holberton School!\n');
-});
+const app = http.createServer();
 
-app.listen(1245);
+app.on('request', (_, res) => {
+  const responseText = 'Hello Holberton School!';
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
+});
 
 module.exports = app;
