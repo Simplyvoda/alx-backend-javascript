9c33a98ec52167931bcc6a55c2e3c0637971a127
Vodina  Efem <efem.vodina@gmail.com>
2024-05-01 06:30:57 -0700

    Create 2-read_file.js

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+/**
+ * Counts the number of students in the database
+ * @param : datapath
+ */
+
+const countStudents = (path) => {
+  if (!fs.existsSync(path)) {
+    throw new Error('Cannot load the database');
+  }
+  if (!fs.statSync(data).isFile()) {
+    throw new Error('Cannot load the database');
+  }
+  
+  try{
+    // reading file synchronously
+    const data = fs.readFileSync(path, 'utf-8')
+    const lines = data.trim().split('\n');
+    process.stdout.write(`Number of students: ${lines.length -1}\n`)
+    const obj = {}
+    lines.map((line, index) => {
+      if (index!==0){
+        const seperated = line.split(",")
+        firstName = seperated[0]
+        field = seperated[seperated.length-1].trim()
+        if (!obj[field]){
+          obj[field] = []
+        }
+        obj[field].push(firstName)
+  
+      }
+      return obj
+    })
+    for (const [key, value] of Object.entries(obj)){
+      const studentList = value.join(', ');
+      process.stdout.write(`Number of students in ${key}: ${value.length}. List: ${studentList}\n`)
+    }
+  }catch(err){
+    console.log('Cannot load the database')
+  }
+}
+
+module.exports = countStudents;
