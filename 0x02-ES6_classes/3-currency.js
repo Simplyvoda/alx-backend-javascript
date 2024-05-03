c4d971c7c7d91b65fa77fdb5e506203e702a1fe6
Vodina Efem <vodinaefem@Vodinas-MacBook-Pro.local>
2024-05-03 23:52:30 +0100

    update: fixed eslint errors

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -9,7 +9,7 @@ export default class Currency {
   }
 
   set code(value) {
-    if (typeof value !== "string") {
+    if (typeof value !== 'string') {
       throw new TypeError("Code must be a string");
     }
     this._code = value;
@@ -20,7 +20,7 @@ export default class Currency {
   }
 
   set name(value) {
-    if (typeof value !== "string") {
+    if (typeof value !== 'string') {
       throw new TypeError("Name must be a string");
     }
     this._name = value;
